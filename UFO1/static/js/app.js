318ffd468686a0adeae423481c04625e7c860c8a
Thomas Levy <40259992+tlevy77@users.noreply.github.com>
2021-01-27 16:22:40 -0800

    Add files via upload

---
+++ UFO1/static/js/app.js
@@ -0,0 +1,42 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+var $tbody = d3.select("tbody");
+
+tableData.forEach(function(based) {
+    var row = $tbody.append("tr");
+    Object.entries(based).forEach(function([key,value]) {
+        console.log(key,value)
+        var cell = row.append("td");
+        cell.text(value);
+    });
+ });
+
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter(sight => sight.datetime === inputValue);
+    console.log(inputValue);
+    console.log(tableData);
+    $tbody.html("")
+    
+    if(filteredData.length !== 0) {
+    filteredData.forEach(function(based) {
+        var row = $tbody.append("tr");
+        Object.entries(based).forEach(function([key,value]) {
+            console.log(key,value)
+            var cell = row.append("td");
+            cell.text(value);
+        })
+     })
+    }
+
+    else {
+        var p = $tbody.append("p");
+        p.text("There are no UFO sightings for this date! Try a different date instead!");
+    }
+})
